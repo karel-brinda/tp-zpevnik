aa20736b207a69338790336a29fb021801173a60
Karel Břinda <karel.brinda@gmail.com>
2016-06-06 21:39:18 +0100

    surge: 3 kategorie zpevniku

--- vygeneruj_index_surge.py
+++ vygeneruj_index_surge.py
@@ -5,14 +5,53 @@ import subprocess
 
 cmd='cd output && find . -type f -name "*.pdf" | sort'
 				   
+def html_pisen(fn):
+	return '\n\t<li><a href="{url}">{url}</a></li>'.format(url=fn.replace("./",""))
+
 if __name__ == '__main__':
 	res=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True).stdout.read()
 	fns=str(res)
 	fns=fns.split()
-	lis=['\n\t<li><a href="{url}">{url}</a></li>'.format(url=x.replace("./","")) for x in fns ]
-	table = "<ul>\n" + "".join(lis) + "\n</ul>\n"
+	#list_singles=[html_pisen(url) for url in fns if "TP_zpevnik_komplet_singles" in url]
+	#list_singles=[html_pisen(url) for url in fns if "TP_zpevnik_komplet_singles" in url]
+
+	list_tp_zpevniky=[]
+	list_ostatni_zpevniky=[]
+	list_singles=[]
+
+	for fn in fns:
+		url=html_pisen(fn)
+		if fn.find("TP_zpevnik_komplet_singles")!=-1:
+			list_singles.append(url)
+		elif fn.find("TP_zpevnik")!=-1:
+			list_tp_zpevniky.append(url)
+		else:
+			list_ostatni_zpevniky.append(url)
+
+
+
+	html = """
+		<h3>TP zpěvníky</h3>
+
+		<ul>{tp_zpevniky}
+		</ul>
+
+		<h3>Ostatní zpěvníky</h3>
+
+		<ul>{ostatni_zpevniky}
+		</ul>
+
+		<h3>Singles</h3>
+
+		<ul>{singles}
+		</ul>
+	""".format(
+		tp_zpevniky="".join(list_tp_zpevniky),
+		ostatni_zpevniky="".join(list_ostatni_zpevniky),
+		singles="".join(list_singles),
+	)
 
 	with open('index_template.html') as f:
 		content = f.read()
 
-	print(content.replace("##zpevniky##",table))
+	print(content.replace("##zpevniky##",html))
