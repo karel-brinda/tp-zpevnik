6a4b8e4da61b9f4f0d0f4f2ccb8c61740dc83a4e
Karel Břinda <karel.brinda@gmail.com>
2016-06-28 23:00:03 +0200

    [surge] Správné řazení pro šablonu nového zpěvníku

--- vygeneruj_novy_zpevnik.py
+++ vygeneruj_novy_zpevnik.py
@@ -3,10 +3,20 @@
 import snakemake
 import subprocess
 
+import locale
+
+try:
+	locale.setlocale(locale.LC_ALL,'czech')
+except:
+	locale.setlocale(locale.LC_ALL,'cs_CZ.UTF-8')
+
+
 cmd="find tp-songs -type f -name '*.tex'"
 
 res=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True).stdout.read()
 fns=str(res)
+fns=fns.split("\n")
+fns=sorted(fns, key=locale.strxfrm)
 
 print("""# -*-coding: utf-8 -*-
 chordbook="jmeno_souboru"
@@ -19,7 +29,7 @@ options = [
 
 songs=[""")
 
-for fn in fns.split("\n"):
+for fn in fns:
 	#if fn is not None:
 	if fn!="":
 		print('#\t("{}",0),'.format(fn))
