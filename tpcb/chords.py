0955c74b606f722d2e585e9e4e860faeb4082aea
Karel Břinda <karel.brinda@gmail.com>
2020-03-22 23:07:45 -0400

    Yapf

--- tpcb/chords.py
+++ tpcb/chords.py
@@ -2,60 +2,65 @@
 
 import re
 
-tone_normalization={
-    "Cb" : "H",
-    "C"  : "C",
-    "C#" : "C#",
-    "Db" : "C#",
-    "D"  : "D",
-    "D#" : "D#",
-    "Eb" : "D#",
-    "E"  : "E",
-    "Fb" : "E",
-    "E#" : "F",
-    "F"  : "F",
-    "F#" : "F#",
-    "Gb" : "F#",
-    "G"  : "G",
-    "G#" : "G#",
-    "Ab" : "G#",
-    "A"  : "A",
-    "A#" : "B",
-    "Bb" : "B",
-    "B"  : "B",
-    "B#" : "C",
-    "Hb" : "B",
-    "H"  : "H",
-    "H#" : "C",
+tone_normalization = {
+    "Cb": "H",
+    "C": "C",
+    "C#": "C#",
+    "Db": "C#",
+    "D": "D",
+    "D#": "D#",
+    "Eb": "D#",
+    "E": "E",
+    "Fb": "E",
+    "E#": "F",
+    "F": "F",
+    "F#": "F#",
+    "Gb": "F#",
+    "G": "G",
+    "G#": "G#",
+    "Ab": "G#",
+    "A": "A",
+    "A#": "B",
+    "Bb": "B",
+    "B": "B",
+    "B#": "C",
+    "Hb": "B",
+    "H": "H",
+    "H#": "C",
 }
+
 
 # from chords import *
 # transposition_tone("Hb",3)
 # transposition_chord("Hb",3)
-def transposition_tone(tone,shift):
-    semitones=["C","C#","D","D#","E","F","F#","G","G#","A","B","H"]
-    dict_shifts=dict(
-        [ [semitones[i],semitones[(i+shift) % len(semitones)]] for i in range(len(semitones)) ] +
-        [ ["",""] ]
-    )
-    normalized_tone=tone_normalization[tone]
+def transposition_tone(tone, shift):
+    semitones = [
+        "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "B", "H"
+    ]
+    dict_shifts = dict(
+        [[semitones[i], semitones[(i + shift) % len(semitones)]]
+         for i in range(len(semitones))] + [["", ""]])
+    normalized_tone = tone_normalization[tone]
     return dict_shifts[normalized_tone]
 
-def transposition_chord(chord,shift):
-    semitones=tone_normalization.keys()
 
-    if chord.find(" ")!=-1:
-        raise(NotImplementedError("Unimplemented format of the chord '{}'. Chords cannot contain any spaces.".format(chord)))
+def transposition_chord(chord, shift):
+    semitones = tone_normalization.keys()
+
+    if chord.find(" ") != -1:
+        raise (NotImplementedError(
+            "Unimplemented format of the chord '{}'. Chords cannot contain any spaces."
+            .format(chord)))
 
     #bass tone
-    if len(chord)>2 and chord[-2]=="/":
-        bass_semitone=chord[-1:]
-        chord=chord[:-2]
-    elif len(chord)>3 and  chord[-3]=="/":
-        bass_semitone=chord[-2:]
-        chord=chord[:-3]
+    if len(chord) > 2 and chord[-2] == "/":
+        bass_semitone = chord[-1:]
+        chord = chord[:-2]
+    elif len(chord) > 3 and chord[-3] == "/":
+        bass_semitone = chord[-2:]
+        chord = chord[:-3]
     else:
-        bass_semitone=""
+        bass_semitone = ""
 
     if chord[:2] in semitones:
         main_semitone = chord[:2]
@@ -64,16 +69,18 @@ def transposition_chord(chord,shift):
         main_semitone = chord[:1]
         middle = chord[1:]
     else:
-        raise(NotImplementedError("Unimplemented format of the chord '{}'".format(chord)))
+        raise (NotImplementedError(
+            "Unimplemented format of the chord '{}'".format(chord)))
 
-    new_chord = transposition_tone(main_semitone,shift) + middle
+    new_chord = transposition_tone(main_semitone, shift) + middle
     if bass_semitone != "":
-        new_chord += "/"+transposition_tone(bass_semitone,shift)
+        new_chord += "/" + transposition_tone(bass_semitone, shift)
 
     return new_chord
 
+
 # transposition_song("""\Ch{Cmi}{Někdy} se \Ch{Fmi}{zajikáme} \Ch{Cmi}{štěstím,}""",3)
-def transposition_song(text,shift):
+def transposition_song(text, shift):
     lines = text.split("\n")
     for i in range(0, len(lines)):
         parts = lines[i].split("<")
@@ -84,8 +91,8 @@ def transposition_song(text,shift):
             # Pokud je těsně za akordem mezera, ukončit jej hned, aby "visel
             # ve vzduchu" a nepřekrýval se s následujícími slovy. Toto je
             # konzistentní s akordem umístěným na konec řádky.
-            if parts[j].find(" ", right_index+1) == right_index+1:
-                end_index = right_index+1
+            if parts[j].find(" ", right_index + 1) == right_index + 1:
+                end_index = right_index + 1
                 star = False
             else:
                 # Pokud je v řetězci některý ze speciálních znaků \, } nebo &
@@ -95,9 +102,10 @@ def transposition_song(text,shift):
                 if end_match == None:
                     end_index = re.search("\s*$", parts[j]).start()
                     # Detekuje přerušení uprostřed slova
-                    star = (end_index == len(parts[j]) and j < len(parts)-1)
+                    star = (end_index == len(parts[j]) and j < len(parts) - 1)
                 else:
-                    end_index = re.search("\s*$", parts[j][:end_match.start()]).start()
+                    end_index = re.search(
+                        "\s*$", parts[j][:end_match.start()]).start()
                     star = False
             if end_index != -1:
                 parts[j] = ("\Ch*" if star else "\Ch") + "{" + chord + "}{" + \
@@ -109,17 +117,20 @@ def transposition_song(text,shift):
     text = "\n".join(lines)
 
     parts = text.split("\Ch")
-    for i in range(1,len(parts)):
+    for i in range(1, len(parts)):
         #print("part", parts[i])
-        mod_part=parts[i].lstrip()
+        mod_part = parts[i].lstrip()
         #assert parts[i][0]=="{"
-        star=(parts[i][0]=="*")
-        left_index=mod_part.find("{")
-        right_index=mod_part.find("}")
-        original_chord=mod_part[left_index+1:right_index].strip()
+        star = (parts[i][0] == "*")
+        left_index = mod_part.find("{")
+        right_index = mod_part.find("}")
+        original_chord = mod_part[left_index + 1:right_index].strip()
         #print("original chord", original_chord)
-        assert original_chord.find("\\")==-1,"Chord '{}' contains forbidden character '\\''".format(original_chord)
-        new_chord=transposition_chord(original_chord,shift)
-        parts[i]=("*{" if star else "{")+new_chord+mod_part[right_index:]
+        assert original_chord.find(
+            "\\"
+        ) == -1, "Chord '{}' contains forbidden character '\\''".format(
+            original_chord)
+        new_chord = transposition_chord(original_chord, shift)
+        parts[i] = ("*{" if star else "{") + new_chord + mod_part[right_index:]
 
     return "\Ch".join(parts)
--- vygeneruj_index_surge.py
+++ vygeneruj_index_surge.py
@@ -5,43 +5,45 @@ import subprocess
 import locale
 
 try:
-	locale.setlocale(locale.LC_ALL,'czech')
+    locale.setlocale(locale.LC_ALL, 'czech')
 except:
-	locale.setlocale(locale.LC_ALL,'cs_CZ.UTF-8')
+    locale.setlocale(locale.LC_ALL, 'cs_CZ.UTF-8')
+
+cmd = 'cd output && find . -type f -name "*.pdf" | grep -v "muj_novy_zpevnik"'
 
 
-cmd='cd output && find . -type f -name "*.pdf" | grep -v "muj_novy_zpevnik"'
-				   
 def html_pisen(fn):
-	fn=fn.replace("./","")
-	return '\n\t<li><a href="{url}">{name}</a></li>'.format(
-			url=fn,
-			name=fn.replace("TP_zpevnik_komplet_singles/",""),
-		)
+    fn = fn.replace("./", "")
+    return '\n\t<li><a href="{url}">{name}</a></li>'.format(
+        url=fn,
+        name=fn.replace("TP_zpevnik_komplet_singles/", ""),
+    )
+
 
 if __name__ == '__main__':
-	res=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True).stdout.read()
-	fns=str(res)
-	fns=fns.split()
+    res = subprocess.Popen(cmd,
+                           shell=True,
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True).stdout.read()
+    fns = str(res)
+    fns = fns.split()
 
-	fns = sorted(fns, key=locale.strxfrm)
+    fns = sorted(fns, key=locale.strxfrm)
 
-	list_tp_zpevniky=[]
-	list_ostatni_zpevniky=[]
-	list_singles=[]
+    list_tp_zpevniky = []
+    list_ostatni_zpevniky = []
+    list_singles = []
 
-	for fn in fns:
-		url=html_pisen(fn)
-		if fn.find("TP_zpevnik_komplet_singles")!=-1:
-			list_singles.append(url)
-		elif fn.find("TP_zpevnik")!=-1:
-			list_tp_zpevniky.append(url)
-		else:
-			list_ostatni_zpevniky.append(url)
+    for fn in fns:
+        url = html_pisen(fn)
+        if fn.find("TP_zpevnik_komplet_singles") != -1:
+            list_singles.append(url)
+        elif fn.find("TP_zpevnik") != -1:
+            list_tp_zpevniky.append(url)
+        else:
+            list_ostatni_zpevniky.append(url)
 
-
-
-	html = """
+    html = """
 		<h3>TP zpěvníky</h3>
 
 		<ul>{tp_zpevniky}
@@ -57,12 +59,12 @@ if __name__ == '__main__':
 		<ul>{singles}
 		</ul>
 	""".format(
-		tp_zpevniky="".join(list_tp_zpevniky),
-		ostatni_zpevniky="".join(list_ostatni_zpevniky),
-		singles="".join(list_singles),
-	)
+        tp_zpevniky="".join(list_tp_zpevniky),
+        ostatni_zpevniky="".join(list_ostatni_zpevniky),
+        singles="".join(list_singles),
+    )
 
-	with open('index_template.html') as f:
-		content = f.read()
+    with open('index_template.html') as f:
+        content = f.read()
 
-	print(content.replace("##zpevniky##",html))
+    print(content.replace("##zpevniky##", html))
